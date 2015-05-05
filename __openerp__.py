48a5504662cc7f3fc442171afa939f6bcc9af22c
michalsubrt <michal.subrt.info@gmail.com>
2015-05-05 15:54:05 +0200

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Transport Purchase",
+    "version": "1.0",
+    "depends": ["base","fleet","sale","hr","purchase"],
+    "author": "",
+    'website':'',
+    "category": "Sales Management",
+    "description":"",
+    'data':['transport_purchase.xml','security/ir.model.access.csv'],
+    'installable': True,
+}
